60774b6866a7da514520fc9af32337f7fef778e6
rdmaR-05 <157958624+rdmaR-05@users.noreply.github.com>
2025-04-11 00:15:33 +0530

    Update downloadcsv.py

--- downloadcsv.py
+++ downloadcsv.py
@@ -1,20 +1,12 @@
 import time
 import pyautogui
-
-# Set the duration for 5 minutes (300 seconds)
 end_time = time.time() + 300  
-
-pyautogui.click(1178, 1056)  # Click first location
+pyautogui.click(1178, 1056)  
 while time.time() < end_time:
-    pyautogui.click(570, 446)    # Click second location
-    time.sleep(1)  # Wait for UI to update
-
-    pyautogui.press('down')  # Press down key to select the option
+    pyautogui.click(570, 446)   
+    time.sleep(1) 
+    pyautogui.press('down') 
     time.sleep(1)
     pyautogui.press('enter')
-
-    pyautogui.click(1652, 525)  # Click third location
-
-    time.sleep(1)  # Add delay between loops to avoid excessive clicking
-
-print("Clicking stopped after 5 minutes.")
+    pyautogui.click(1652, 525)  
+    time.sleep(1) 
